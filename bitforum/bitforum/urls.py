683a2daaa3ad4b25a80f5c098aebb323a9c45f68
zainsarfraz <44726141+zainsarfraz@users.noreply.github.com>
2021-01-04 03:11:35 +0500

    test

--- bitforum/bitforum/urls.py
+++ bitforum/bitforum/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('post/',include(urls)),
     path('profile',profileTest),
     path('login',login_signup_page),
