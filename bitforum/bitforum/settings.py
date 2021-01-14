71bfbe2c7fb62bcf46c1cc9f6cee1ff96fa9da6b
maaz <maaz.ravian@gmail.com>
2021-01-14 17:07:04 +0500

    a

--- bitforum/bitforum/settings.py
+++ bitforum/bitforum/settings.py
@@ -124,8 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 MEDIA_URL = '/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
@@ -137,4 +138,3 @@ STATICFILES_DIRS = [
 
 
 django_heroku.settings(locals())
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
